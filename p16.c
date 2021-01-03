cdd9ff54715c4a3a4c7ff86b9b0f0bbe5fbdee83
“Sunny020720 <1538052461@qq.com>
2021-01-03 23:15:39 +0800

    new

---
+++ p16.c
@@ -0,0 +1,20 @@
+//p16
+#include<stdio.h>
+int main()
+{
+	int height,length,width,volume,weight;
+	
+	printf("enter height of box:");
+	scanf("%d",&height);
+	printf("enter length of box:");
+	scanf("%d",&length);
+	printf("enter width of box:");
+	scanf("%d",&width);
+	volume = height*length*width;
+	weight=(volume+165)/166;
+	
+	printf("Volume: %d\n",volume);
+	printf("dimensional weight:%d\n",weight);
+
+    return 0;
+}
